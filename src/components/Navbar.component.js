9897ab793781884948c810be7c0b341567d5964d
brandon-kyle-bailey <brandonkylebailey@outlook.com>
2020-02-02 17:45:43 +0000

    structuring initial app with create react app

---
+++ src/components/Navbar.component.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../Navbar.css';
+
+function Navbar() {
+    return ( 
+    <div className="Navbar">
+        <h1>This will be a navigation bar.</h1> 
+    </div>
+    )
+}
+
+
+export default Navbar;
