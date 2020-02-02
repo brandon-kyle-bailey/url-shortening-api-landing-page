3b545785d0a2fd380b95631fb061267549327ea2
brandon-kyle-bailey <brandonkylebailey@outlook.com>
2020-02-02 16:59:21 +0000

    Updates

---
+++ precache-manifest.01ee53fcdc590555495315ac24158b80.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7721377b1029370cd16d8ba11ab12c9",
+    "url": "/url-shortening-api-landing-page/index.html"
+  },
+  {
+    "revision": "ef93546ed4ee14231bad",
+    "url": "/url-shortening-api-landing-page/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "871854e3b2747a7d6ed6",
+    "url": "/url-shortening-api-landing-page/static/js/2.1045fb09.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/url-shortening-api-landing-page/static/js/2.1045fb09.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef93546ed4ee14231bad",
+    "url": "/url-shortening-api-landing-page/static/js/main.eb118c47.chunk.js"
+  },
+  {
+    "revision": "60b30e5b761880a03af9",
+    "url": "/url-shortening-api-landing-page/static/js/runtime-main.1b9acbfa.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/url-shortening-api-landing-page/static/media/logo.5d5d9eef.svg"
+  }
+]);
